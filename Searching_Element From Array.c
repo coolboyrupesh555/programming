4218fab05f24224d0bbb53f03d4744aedf969510
Rupesh Rajbhar <rajbharrupesh2@gmail.com>
2017-08-14 08:31:25 -0700

    Update Searching_Element From Array.c

--- Searching_Element From Array.c
+++ Searching_Element From Array.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 
-void search(){
-
-	int array[50],i,location,find,size;
-	printf("\n Enter the Size Of Array:");
+void exlogic(){
+	
+	int array[50],i,find,size,temp,index;
+	printf("\n Enter the Size Of Array max(50)");
 	scanf("%d",&size);
-
+	
 	printf("\n Enter the Element In Array:");
 	for(i=0; i<size; i++){
 		scanf("%d",&array[i]);
 	}
-
-	printf("\n Enter the  Value Which You Want To Find");
+	
+	printf("\n Enter the Value Which YOu Want To find\n");
 	scanf("%d",&find);
+	
 	for(i=0; i<size; i++){
+		
 		if(array[i] == find){
-
-			printf("\nIndex:%d Value %d",i,find);
-
+			temp = find;
+			index = i;
+			break;
 		}
+		
+		}
+		
+	if(temp  == find){
+		
+		printf("the index of that element is :%d\n,%d",index,find);		
+	}else{
+		printf("Match is not found Try Again");
 	}
+
 }
 
 int main(){
-	search();
-
+	exlogic();
+	
 	return 0;
 }
